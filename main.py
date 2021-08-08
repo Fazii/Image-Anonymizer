f92610443c29f0d4e38391d1ac2d05fece91d747
fazii <krzysiek459@onet.eu>
2021-08-08 18:03:34 +0200

    Add data anonymization

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 #!/usr/bin/env python
+
+import math
+
 import base64
-import threading
-import glob
-import numpy as np
-from kafka import KafkaProducer
 import cv2
+import json
+import numpy as np
+import threading
+from kafka import KafkaConsumer
+from kafka import KafkaProducer
 
 
-class Producer(threading.Thread):
+class Anonymizer(threading.Thread):
+
     def __init__(self):
         threading.Thread.__init__(self)
         self.stop_event = threading.Event()
@@ -17,37 +22,70 @@ class Producer(threading.Thread):
 
     def run(self):
         producer = KafkaProducer(bootstrap_servers='localhost:9092')
+        consumer = KafkaConsumer(bootstrap_servers='localhost:9092',
+                                 auto_offset_reset='latest',
+                                 consumer_timeout_ms=1000)
 
-        images = glob.glob("data/*.jpg")
+        consumer.subscribe(['input-topic'])
 
-        # cv2.bitwise_not
-        for image in images:
-            image = cv2.imread(image)
-            hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-            lower = np.array([0, 0, 218])
-            upper = np.array([157, 54, 255])
-            mask = cv2.inRange(hsv, lower, upper)
+        while not self.stop_event.is_set():
+            for message in consumer:
+                print(message)
+                data = json.loads(str(message.value)[2:-1])
+                anonymizedImage = self.anonymizeImage(data['image'])
+                anonymizedData = self.anonymizeData(data['data'])
+                anonymized = {
+                    'data': anonymizedData,
+                    'image': anonymizedImage.decode(),
+                }
+                anonymizedJson = json.dumps(anonymized)
+                producer.send('output-topic', anonymizedJson.encode('utf-8'))
 
-            kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 3))
-            dilate = cv2.dilate(mask, kernel, iterations=5)
+                if self.stop_event.is_set():
+                    break
 
-            cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            cnts = cnts[0] if len(cnts) == 2 else cnts[1]
-            for c in cnts:
-                x, y, w, h = cv2.boundingRect(c)
-                ar = w / float(h)
-                if ar > 5:
-                    cv2.drawContours(image, [c], -1, (0, 0, 0), -1)
-            _, buffer_image = cv2.imencode('.jpg', image)
-            encoded = base64.b64encode(buffer_image)
-            producer.send('my-topic', encoded)
-
+        consumer.close()
         producer.close(1000)
+
+    def anonymizeImage(self, inputImg):
+        image = base64.b64decode(inputImg)
+        jpg_as_np = np.frombuffer(image, dtype=np.uint8)
+        image = cv2.imdecode(jpg_as_np, flags=1)
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        lower = np.array([0, 0, 218])
+        upper = np.array([157, 54, 255])
+        mask = cv2.inRange(hsv, lower, upper)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 3))
+        dilate = cv2.dilate(mask, kernel, iterations=5)
+        cnts = cv2.findContours(dilate, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+        for c in cnts:
+            x, y, w, h = cv2.boundingRect(c)
+            ar = w / float(h)
+            if ar > 5:
+                cv2.drawContours(image, [c], -1, (0, 0, 0), -1)
+        _, buffer_image = cv2.imencode('.jpg', image)
+        encoded = base64.b64encode(buffer_image)
+        return encoded
+
+    def anonymizeData(self, inputData):
+        anonymizedData = {
+            'wiek': getInterval(inputData['wiek']),
+            'wzrost': getInterval(inputData['wzrost']),
+            'waga': getInterval(inputData['waga']),
+            'choroba': inputData['choroba']
+        }
+        return anonymizedData
+
+
+def getInterval(x):
+    floor = int(math.floor(int(x) / 10.0)) * 10
+    return "[" + str(floor) + "-" + str(floor + 10) + "]"
 
 
 def main():
-    producer = Producer()
-    producer.start()
+    anonymizer = Anonymizer()
+    anonymizer.start()
 
 
 if __name__ == "__main__":
